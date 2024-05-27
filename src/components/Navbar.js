181a35f0c4eb063cafd55afdafd6d28afccc625a
VaibhavCoder123 <129491099+VaibhavCoder123@users.noreply.github.com>
2024-05-27 17:37:02 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -8,9 +8,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -25,24 +25,30 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
+              {/* <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a> */}
             </li>
             <li className="nav-item">
-              {/* <Link className="nav-link" to="/about">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </Link> */}
+              </Link>
             </li>
           </ul>
           <form
-            className={`d-flex  border-${props.mode === "light" ? "dark" : "light"} role="search"`}
+            className={`d-flex  border-${
+              props.mode === "light" ? "dark" : "light"
+            } role="search"`}
           >
             <input
               className="form-control me-2"
               type="search"
-              style={{backgroundColor: props.mode === "light" ? "white" : "#191818",
-              color: props.mode === "light" ? "black" : "white",
+              style={{
+                backgroundColor: props.mode === "light" ? "white" : "#191818",
+                color: props.mode === "light" ? "black" : "white",
               }}
               placeholder="Search"
               aria-label="Search "
@@ -71,7 +77,7 @@ export default function Navbar(props) {
               className="form-check-label"
               htmlFor="flexSwitchCheckDefault"
             >
-              Enable DarkMode
+              Toggle Mode
             </label>
           </div>
         </div>
